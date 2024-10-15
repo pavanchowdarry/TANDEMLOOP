209462ebff5e5be91e14c336f4603beb560e4660
pavanchowdarry <129689307+pavanchowdarry@users.noreply.github.com>
2024-10-15 21:16:24 +0530

    Create program-3.java

---
+++ program-3.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class OddNumberSeries {
+
+    public static void generateSeries(int a) {
+        int limit = (a % 2 == 0) ? a - 1 : a;
+        int oddNumber = 1;
+        for (int i = 1; i <= limit; i++) {
+            System.out.print(oddNumber);
+            if (i < limit) {
+                System.out.print(", ");
+            }
+            oddNumber += 2;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of a: ");
+        int a = sc.nextInt();
+        generateSeries(a);
+        sc.close();
+    }
+}
